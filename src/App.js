88cf60c898ec6600421ff4afa200519daf72e0b3
kareem42 <kareem4209@gmail.com>
2025-03-30 20:47:54 -0500

    created the buttons and search bar for the topbar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBasline, ThemeProvider } from "@mui/material";
+import { Topbar } from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
 
   return (
-  <ColorModeContext.Provider value={colorMode}>
-    <ThemeProvider theme = {theme}>
-      <CssBasline>
-    <div className="App">
-      <main className="content"></main>
-    </div>
-     </CssBasline>
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBasline>
+          <div className="App">
+            <main className="content">
+              <Topbar />
+            </main>
+          </div>
+        </CssBasline>
+      </ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
